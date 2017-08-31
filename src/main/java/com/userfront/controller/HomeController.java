492855f95f6f8ac84a2a23b796038f2e20eb00b1
AndreyVoytuk <andrey.voytuk87@gmail>
2017-08-31 14:44:15 +0300

    Added some static files, home and signup html files

---
+++ src/main/java/com/userfront/controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.userfront.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.userfront.domain.User;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "redirect:/index";
+	}
+	
+	@RequestMapping("/index")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping(value="/signup")
+	public String signup(Model model) {
+		User user = new User();
+		model.addAttribute("user", user);
+		
+		return "signup";
+	}
+}
